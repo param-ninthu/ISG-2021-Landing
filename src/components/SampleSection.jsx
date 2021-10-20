25525616c872bf8afbd187a9966c331a087fd1d8
Sandul Renuja <sandulrenuja@gmail.com>
2021-10-20 14:16:25 +0530

    Add common assets and resources
    Added main ud-styles file from Play Bootstrap template.
    Added SASS files for common styles and variables.
    Added a sample React FC file.

---
+++ src/components/SampleSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SampleSection = () => {
+  return (
+    <section>
+      This is a React Function Component for a sample section
+    </section>
+  );
+};
+
+export default SampleSection;
