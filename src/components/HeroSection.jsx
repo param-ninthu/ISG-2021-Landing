7ac3e87646989d576a994e9dc3bdcbe8d5eff841
Sandul Renuja <sandulrenuja@gmail.com>
2021-10-28 21:14:29 +0530

    Add flip countdown

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Col, Container, Row} from "react-bootstrap";
 import logo from "../assets/images/ics-logo.png"
+import FlipCountdown from '@rumess/react-flip-countdown';
 
 const HeroSection = () => {
   return (
@@ -9,20 +10,34 @@ const HeroSection = () => {
         <Row>
           <Col lg={12} className="mb-5 pb-4">
             <div className="ud-hero-content wow fadeInUp m-auto" data-wow-delay=".2s">
-              <img src={logo} alt="ISG Logo" className="mb-3 mb-md-2 mb-lg-1 mb-xxl-3 p-1 p-xxl-2 ud-hero-logo"/>
+              <img src={logo} alt="ISG Logo" className="mb-3 mb-md-2 mb-lg-1 mb-xxl-2 p-0 ud-hero-logo"/>
               <h1 className="ud-hero-title my-3 mb-4 mb-lg-0">
                 iCS - Hack The World
               </h1>
-              <h3 className="ud-hero-subtitle pb-lg-1 my-3 my-md-2 my-xxl-3 px-4">24-hour Capture The Flag (CTF)
+              <h3 className="ud-hero-subtitle pb-lg-1 my-3 my-md-2 mb-xxl-3 px-4">24-hour Capture The Flag (CTF)
                 Competition</h3>
-              {/*<p className="ud-hero-desc mt-3">*/}
-              {/*  00 days&ensp;00 hours&ensp;00 mins*/}
-              {/*</p>*/}
-              <div className="ud-hero-buttons my-2 mt-5 mt-sm-3 mt-lg-4 mt-xxl-5">
-                <a href="/" rel="nofollow noopener" target="_blank"
-                   className="ud-main-btn ud-white-btn mx-3">
-                  Register Now
-                </a>
+              <p className="ud-hero-desc mt-3 mt-xxl-4 p-2">
+                Registrations open in
+              </p>
+              <div className="ud-hero-buttons my-2 mt-5 mt-sm-3 mt-lg-3 mt-xxl-4">
+                {/*<a href="/" rel="nofollow noopener" target="_blank"*/}
+                {/*   className="ud-main-btn ud-white-btn mx-3">*/}
+                {/*  Register Now*/}
+                {/*</a>*/}
+
+                <FlipCountdown
+                  theme="dark"
+                  hideYear
+                  hideMonth
+                  endAtZero
+                  titlePosition="bottom"
+                  dayTitle='Days'
+                  hourTitle='Hours'
+                  minuteTitle='Minutes'
+                  secondTitle='Seconds'
+                  endAt={"2021-11-15 00:00:00"} // Date/Time
+                />
+
               </div>
             </div>
             {/*<div className="ud-hero-image wow fadeInUp" data-wow-delay=".25s">*/}
