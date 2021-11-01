3d4988287270f35a5f7ae72c9bf84c5e3bdb7f83
Sandul Renuja <sandulrenuja@gmail.com>
2021-11-01 20:38:23 +0530

    Update navbar and add back-to-top button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,21 @@
 import React, {useState} from 'react'
 import logo from '../pictures/logo/logo.png'
 import {Col, Container, Navbar, Row} from 'react-bootstrap';
+import {BsChevronUp, FaChevronUp} from "react-icons/all";
 
 const NavBar = () => {
-    let [scrollStyle = "ud-header", setScrollStyle] = useState();
-    let [isActive, setActive] = useState(false);
+    const [scrollStyle, setScrollStyle] = useState("ud-header");
+    const [isActive, setActive] = useState(false);
+    const [isBackToTopShown, setIsBackToTopShown] = useState(false);
 
     window.onscroll = () => {
-        if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-            setScrollStyle(scrollStyle = "ud-header sticky")
+        if (document.body.scrollTop > 2 || document.documentElement.scrollTop > 2) {
+            setScrollStyle("ud-header sticky");
+            setIsBackToTopShown(true);
+
         } else {
-            setScrollStyle(scrollStyle = "ud-header")
+            setScrollStyle("ud-header")
+            setIsBackToTopShown(false);
         }
     }
 
@@ -19,55 +24,60 @@ const NavBar = () => {
     }
 
     return (
-        <header className={scrollStyle}>
-            <Container>
-                <Row>
-                    <Col lg={12}>
-                        <Navbar className="navbar" expand="lg">
-                            <Navbar.Brand className="navbar-brand" href="#home">
-                                <img src={logo} alt="logo"/>
-                            </Navbar.Brand>
-                            <button className={isActive ? "navbar-toggler active" : "navbar-toggler"}
-                                    onClick={navbarToggle}>
-                                <span className="toggler-icon"> </span>
-                                <span className="toggler-icon"> </span>
-                                <span className="toggler-icon"> </span>
-                            </button>
-                            <div className={isActive ? "navbar-collapse show" : "navbar-collapse"}>
-                                <ul id="nav" className="navbar-nav mx-auto">
-                                    <li className="nav-item">
-                                        <a className="ud-menu-scroll" href="#home">Home</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="ud-menu-scroll" href="#about">About</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a href="#pricing">Schedule</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="ud-menu-scroll" href="#team">Register</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="ud-menu-scroll" href="#contact">Awards</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="ud-menu-scroll" href="#contact">Past Events</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="ud-menu-scroll" href="#contact">Contact</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="navbar-btn d-none d-sm-inline-block">
-                                <a className="ud-main-btn ud-white-btn" href="javascript:void(0)">
-                                    Register
-                                </a>
-                            </div>
-                        </Navbar>
-                    </Col>
-                </Row>
-            </Container>
-        </header>
+        <>
+            <a href="#" className="back-to-top" style={isBackToTopShown ? {opacity: 1} : {opacity: 0}}>
+                <FaChevronUp/>
+            </a>
+            <header className={scrollStyle}>
+                <Container>
+                    <Row>
+                        <Col lg={12}>
+                            <Navbar className="navbar" expand="lg">
+                                <Navbar.Brand className="navbar-brand" href="#home">
+                                    <img src={logo} alt="logo"/>
+                                </Navbar.Brand>
+                                <button className={isActive ? "navbar-toggler active" : "navbar-toggler"}
+                                        onClick={navbarToggle}>
+                                    <span className="toggler-icon"> </span>
+                                    <span className="toggler-icon"> </span>
+                                    <span className="toggler-icon"> </span>
+                                </button>
+                                <div className={isActive ? "navbar-collapse show" : "navbar-collapse"}>
+                                    <ul id="nav" className="navbar-nav mx-auto">
+                                        <li className="nav-item">
+                                            <a className="ud-menu-scroll" href="#home">Home</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="ud-menu-scroll" href="#about">About</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a href="#pricing">Schedule</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="ud-menu-scroll" href="#team">Register</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="ud-menu-scroll" href="#contact">Awards</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="ud-menu-scroll" href="#contact">Past Events</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="ud-menu-scroll" href="#contact">Contact</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="navbar-btn d-none d-sm-inline-block">
+                                    <a className="ud-main-btn ud-white-btn" href="javascript:void(0)">
+                                        Register
+                                    </a>
+                                </div>
+                            </Navbar>
+                        </Col>
+                    </Row>
+                </Container>
+            </header>
+        </>
     )
 }
 
