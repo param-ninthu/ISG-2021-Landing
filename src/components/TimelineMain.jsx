405ecbe63811e3027b60aeae3f731a125f1523aa
Dhatchaya <dhatchaya2000@gmail.com>
2021-11-01 18:16:52 +0530

    update timeline

--- src/components/TimelineMain.jsx
+++ src/components/TimelineMain.jsx
@@ -2,65 +2,82 @@ import React from 'react';
 import Timeline2 from './Timeline2';
 import Timeline1 from './Timeline1';
 import '../assets/scss/_timeline.scss';
+import Nav from 'react-bootstrap/Nav';
 
 class Timeline extends React.Component {
-
+    
     state = {
         div1Shown: true,
-        counter: 0,
-        changeArrow: false,
-    }
+        div2Shown: false,
 
-    handleButtonClick() {
+      }
+    
+      handleButtonClick() {
         this.setState({
-            div1Shown: (!this.state.div1Shown),
-            counter: (!this.state.counter),
-            changeArrow: (!this.state.changeArrow),
+          div1Shown: true,
+          div2Shown: false,
+
+
         });
 
-    }
-
-    Date() {
-        const value = this.state.counter;
-        return value ? "DAY 2" : "DAY 1";
-    }
-
+       
+         
+      }
+      handleButtonClick2(){
+          this.setState({
+              div2shown:true,
+              div1Shown:false,
+          })
+      }
+    
     render() {
         return (
-
-            <section className="head" style={{background: 'black'}}>
-                <div className="head_title">
-                    <div className="containers">
-                        <div className="group">
-                            <span className='bot' id="top"/>
+           
+            <section className="head" style={{ background : 'black'}}>
+                 <div className="head_title"> 
+                    <Nav variant="pills" defaultActiveKey={1}  >
+                    <Nav.Item >
+                    <Nav.Link eventKey={1} onClick={() => this.handleButtonClick()}>
+                        <div className="containers" id="#target" >
+                            <div className="group">
+                            <span className='bot' id="top"></span>
                             <div className="box">
-                                <div className="item">{this.Date()}</div>
+                            <div className="item">DAY 1</div>
                             </div>
-                            <span className='bot' id="bottom"/>
-                        </div>
-                    </div>
-                    <div className="center-con">
-                        <div onClick={() => this.handleButtonClick()}
-                             className={this.state.changeArrow ? 'changeArrow' : 'round'}
-                        >
-                            <div id="cta">
-                                <span className="arrow primera next "/>
-                                <span className="arrow segunda next "/>
+                            <span className='bot'  id="bottom"></span>
                             </div>
                         </div>
-                    </div>
-
-
+                        </Nav.Link>
+                
+       
+                    </Nav.Item>
+                    <Nav.Item >
+                    <Nav.Link eventKey={2} onClick={() => this.handleButtonClick2()}>
+                        <div className="containers">
+                            <div className="group">
+                            <span className='bot' id="top"></span>
+                            <div className="box">
+                            <div className="item">DAY 2</div>
+                            </div>
+                            <span className='bot'  id="bottom"></span>
+                            </div>
+                        </div>
+                    </Nav.Link>
+                
+                    </Nav.Item>
+                    
+                    </Nav>
                 </div>
+      
                 <div className="timeline">
                     {
-                        this.state.div1Shown ?
-                            (<Timeline1/>)
-                            : (<Timeline2/>)
+                    this.state.div1Shown ? 
+                         (<Timeline1/>) 
+                        :(<Timeline2/>) 
                     }
                 </div>
-
-
+                
+                
             </section>
         );
     }
