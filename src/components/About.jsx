65bdc96a6df839ed822f86913737134b1c98e886
Sandul Renuja <sandulrenuja@gmail.com>
2021-11-01 18:57:18 +0530

    Update about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,91 +1,97 @@
-import React, {} from 'react-bootstrap';
-import logo from "../assets/images/ISACA_STUDENTGROUP_anniv_logo_4C_template.png"
-import '../assets/scss/_About.scss';
+import React, {Col, Container, Row} from 'react-bootstrap';
+import '../assets/scss/_about.scss';
+
 const About = () => {
-    const styleObj = {
-        fontSize: 20,
-        color: "#ffffff",
-        textAlign: "center"
-    }
 
-    return(
-                 <section id="pricing" className="ud-sponsors">
-                            <div className="ud-section-title mx-auto text-center">
-                            <h2 data-aos="zoom-in-up">
-                                    WHAT IS iCS HACK THE WORLD?</h2>
-                            </div>
-                            <div className="Space">
-                            <div data-aos="zoom-in-up">
-                            <div style={styleObj}>
-                            iCS Hack the World is a 24-hour Capture the Flag competition that will put your hacking skills to the ultimate test!
-                             </div>
-                             </div>
-                            </div>
-                             {/* -----------------------------------------/Cards----- */}
-                             <div class="container">
-                               <div class="animated fadeInDown">
-                               <div class="card">
-<div class="card-group">
-  <div class="card mb-4 border-0">
-    <div class="card-body">
-    <div data-aos="fade-right">
-      <h3 class="card-title">Who is it for?</h3>
-      <p class="card-text">Undergraduates from both state universities and private higher education institutes in Sri Lanka.</p>
-      </div>
-    </div>
-  </div>
-  <div class="card mb-4 border-0">
-    <div class="card-body">
-    <div data-aos="fade-down">
-      <h3 class="card-title">When is it?</h3>
-      <p class="card-text">On the 16th January 2021 from 8 am onwards.</p>
-      </div>
-    </div>
-  </div>
-  <div class="card mb-4 border-0">
-    <div class="card-body">
-    <div data-aos="fade-left">
-      <h3 class="card-title">Can I bring backup?</h3>
-      <p class="card-text">It is for teams of 4 so you'd better.</p>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="card-group">
-  <div class="card mb-4 border-0">
-    <div class="card-body">
-    <div data-aos="fade-right">
-      <h3 class="card-title">Where will it take place?</h3>
-      <p class="card-text">Ever heard of Facebook CTF platform?</p>
-      </div>
-    </div>
-  </div>
-  <div class="card mb-4 border-0">
-    <div class="card-body">
-    <div data-aos="fade-up">
-      <h3 class="card-title">No experience?</h3>
-      <p class="card-text">We'll lend a hand. Join the sessions conducted by our sponsors on the 9th January 2021 via the online stream. But that's all the help you're gonna get from our end!</p>
-    </div>
-    </div>
-  </div>
-  <div class="card mb-4 border-0">
-    <div class="card-body">
-    <div data-aos="fade-left">
-      <h3 class="card-title">Will we win anything?</h3>
-      <p class="card-text">Of course! Loads of gifts coming your way</p>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-</div>
-</div>
+    return (
+        <section id="pricing" className="ud-about aw-dark-back py-5">
+            <Container>
+                <Row>
+                    <Col xs={12} data-aos="zoom-in">
+                        <h2 className="neonText mx-auto text-center my-3 p-3">What is iCS Hack The World?</h2>
+                    </Col>
+                    <Col xs={12}>
+                        <h4 className="text-center m-1 mt-4" data-aos="zoom-in-up">
+                            iCS Hack the World is a 24-hour Capture the Flag competition that will put your hacking
+                            skills
+                            to the ultimate test!
+                        </h4>
+                    </Col>
+                </Row>
 
-<div className="LContent">
-                        <div data-aos="fade-left">
-                        So put together a team, have a little pep talk, and get ready to start hacking!
+                <Row className="my-5 py-1">
+                    <Col xs={12}>
+                        <div className="card animated fadeInDown">
+                            <div className="card-group">
+                                <div className="card mb-4 border-0">
+                                    <div className="card-body">
+                                        <div data-aos="fade-right">
+                                            <h3 className="card-title">Who is it for?</h3>
+                                            <p className="card-text">Undergraduates from both state universities and
+                                                private
+                                                higher education institutes in Sri Lanka.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card mb-4 border-0">
+                                    <div className="card-body">
+                                        <div data-aos="fade-right">
+                                            <h3 className="card-title">When is it?</h3>
+                                            <p className="card-text">On the 16th January 2021 from 8 am onwards.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card mb-4 border-0">
+                                    <div className="card-body">
+                                        <div data-aos="fade-right">
+                                            <h3 className="card-title">Can I bring backup?</h3>
+                                            <p className="card-text">It is for teams of 4 so you'd better.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card-group">
+                                <div className="card mb-4 border-0">
+                                    <div className="card-body">
+                                        <div data-aos="fade-right">
+                                            <h3 className="card-title">Where will it take place?</h3>
+                                            <p className="card-text">Ever heard of Facebook CTF platform?</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card mb-4 border-0">
+                                    <div className="card-body">
+                                        <div data-aos="fade-right">
+                                            <h3 className="card-title">No experience?</h3>
+                                            <p className="card-text">We'll lend a hand. Join the sessions conducted by
+                                                our
+                                                sponsors on the 9th January 2021 via the online stream. But that's all
+                                                the
+                                                help you're gonna get from our end!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card mb-4 border-0">
+                                    <div className="card-body">
+                                        <div data-aos="fade-right">
+                                            <h3 className="card-title">Will we win anything?</h3>
+                                            <p className="card-text">Of course! Loads of gifts coming your way</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        </div>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col xs={12}>
+                        <h4 data-aos="fade-up m-4">
+                            So put together a team, have a little pep talk, and get ready to start hacking!
+                        </h4>
+                    </Col>
+                </Row>
+            </Container>
         </section>
 
     );
